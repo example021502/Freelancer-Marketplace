6c0c64713565e902bb9475b6b687a2b0d8e012a1
example021502 <examples021502@gmail.com>
2026-04-17 21:50:56 +0530

    update:17.04.26.21.50: still having issues with posting new project post

--- backend/controllers/newPostController.js
+++ backend/controllers/newPostController.js
@@ -3,11 +3,16 @@ const pool = require("../config/db");
 const newPost = (req, res) => {
   const { form } = req.body;
   const { creator_id, title, description, budget, discount } = form;
-
-  pool.query(
-    "INSERT INTO posts(creator_id, title, description, budget, created_at, discount) VALUES(?,?,?,?,NOW(),?)",
-    [creator_id, title, description, budget, discount],
-  );
+  try {
+    const result = pool.query(
+      "INSERT INTO posts(creator_id, title, description, budget, created_at, discounts) VALUES(?,?,?,?,NOW(),?)",
+      [creator_id, title, description, budget, discount],
+    );
+    res.status(200).json({ message: "Post created successfully" });
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    res.status(500).json({ message: "Database Error!" });
+  }
 };
 
 module.exports = { newPost };
